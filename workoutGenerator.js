4f60d14c0bc1e2f93ff880eb68d0b3c9d2ccf73f
Joshua Ranjit <joshua.ranjit20@gmail.com>
2023-12-16 18:55:19 +0530

    Added workoutOptions object and random selector function

---
+++ workoutGenerator.js
@@ -0,0 +1,19 @@
+const workoutOptions = {
+    pushups: [{ goal: 20, time: 2 }, { goal: 40, time: 4 }, { goal: 60, time: 6 }],
+    pullups: [{ goal: 10, time: 2 }, { goal: 20, time: 4 }, { goal: 30, time: 6 }],
+    skips: [{ goal: 200, time: 3 }, { goal: 400, time: 6 }, { goal: 600, time: 9 }],
+    squats: [{ goal: 30, time: 3 }, { goal: 60, time: 6 }, { goal: 90, time: 9 }],
+    highknees: [{ goal: 2, time: 2 }, { goal: 4, time: 4 }, { goal: 6, time: 6 }], // Time in minutes
+    jumping_jacks: [{ goal: 50, time: 2 }, { goal: 100, time: 4 }, { goal: 150, time: 6 }],
+    lunges: [{ goal: 30, time: 3 }, { goal: 60, time: 6 }, { goal: 90, time: 9 }],
+    burpees: [{ goal: 15, time: 2 }, { goal: 30, time: 4 }, { goal: 45, time: 6 }],
+    mountain_climbers: [{ goal: 2, time: 2 }, { goal: 4, time: 4 }, { goal: 6, time: 6 }], // Time in minutes
+    planks: [{ goal: '1m', time: 1 }, { goal: '2m', time: 2 }, { goal: '3m', time: 3 }] // Goals as duration
+  };
+
+// creating a utility function for randomly selecting an element
+
+const getRandomElement = (array) => {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+};
